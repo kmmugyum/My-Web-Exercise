db0cfa45f7a76bb4af42b162a5ab7dd6b4d5445e
MuGyum Kim <30917465+kmmugyum@users.noreply.github.com>
2024-06-02 06:45:48 +0000

    수정

---
+++ login/login.js
@@ -0,0 +1,6 @@
+document.getElementById('login_button').addEventListener('click', function(event) {
+    // form의 기본 동작을 막음
+    event.preventDefault();
+
+    window.location.href = '/myweb/myweb.html'
+})
